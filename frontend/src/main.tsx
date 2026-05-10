1d7f86165a43109316657de4cdfc3d2b6ba44674
RonkZeDonk <20400064+RonkZeDonk@users.noreply.github.com>
2026-05-10 00:46:37 -0400

    add react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './index.css'
 import ExplorePage from './ExplorePage.tsx'
 
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ExplorePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/explore' element={<ExplorePage />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
