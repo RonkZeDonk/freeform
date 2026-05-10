d495c23b19f85c0f4d538c2816b3d29043ddb3f0
Shaheer Khan <khan56@uoguelph.ca>
2026-05-10 03:09:34 -0400

    feat: added live video feed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react';
-import './App.css'
-import TimeBar from './components/TimeBar';
-import sampleVideo from './sample-5s.mp4';
+import { useState } from "react";
+import "./App.css";
+import TimeBar from "./components/TimeBar";
+import sampleVideo from "./sample-5s.mp4";
 
 function App() {
   const [progress, setProgress] = useState(0);
   // TODO set progress with socket
 
   return (
-    <div className='bg-gray-700 h-screen'>
-      <video src={sampleVideo} autoPlay loop muted className='w-full h-full absolute' />
+    <div className="bg-gray-700 h-screen">
+      <img
+        src={"http://localhost:8000/video_feed"}
+        className="mx-auto h-full"
+      />
       <TimeBar progress={progress} />
     </div>
-  )
+  );
 }
 
 export default App;
